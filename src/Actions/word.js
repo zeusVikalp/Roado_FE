0a4f1ad407a140db4295e8714fc7720566e7a3d2
= <=>
2022-06-04 08:55:06 +0530

    initial commit to client side app

---
+++ src/Actions/word.js
@@ -0,0 +1,33 @@
+// implementing redux actions
+import { addNewWord, getAllWords } from '../Utils/helper'
+
+export const RECEIVE_WORDS = 'RECEIVE_WORDS'
+export const ADD_NEW_WORD = 'ADD_NEW_WORD'
+
+export const receiveWords = (words) => {
+    return {
+        type: RECEIVE_WORDS,
+        words
+    }
+}
+
+export const addWord = (word) => {
+    return {
+        type: ADD_NEW_WORD,
+        word
+    }
+}
+
+export const handleAddWord = (word) => {
+    return dispatch => {
+        return addNewWord(word)
+            .then(word => dispatch(addWord(word)))
+    }
+}
+
+export const handleInitialData = (words) => {
+    return dispatch => {
+        return getAllWords()
+            .then(words => dispatch(receiveWords(words)))
+    }
+}
